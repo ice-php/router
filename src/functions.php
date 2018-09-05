85d6bb57309a350d6b2f200e117f816ebd5bb965
lanbing <liyong@nenu.edu.cn>
2018-09-05 10:06:36 +0800

    路由管理

---
+++ src/functions.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace icePHP;
+
+/**
+ * 根据控制器名称,动作名称及参数构造URL
+ * 这是Router的一个快捷入口
+ *
+ * @param string $module 模块名称
+ * @param string $controller 控制器名称
+ * @param string $action 动作名称
+ * @param array $params 参数表
+ * @return string 路径字符串,不带域名部分
+ */
+function url(string $module = '', string $controller = "", string $action = "", array $params = []): string
+{
+    return Router::encode($module, $controller, $action, $params);
+}
